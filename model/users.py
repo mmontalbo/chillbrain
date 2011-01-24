244e16189dcc019c340633133593748cf6273c04
Simon Levy <simonlevy5@gmail.com>
2011-01-23 20:54:12 -0800

    Added linkbacks to user and in an update to that value in a share transaction

--- model/users.py
+++ model/users.py
@@ -40,6 +40,11 @@ class ChillUser(BaseUser):
     access_token = db.StringProperty(required=True)
     shares = db.ListProperty(db.Key) # List of transaction IDs that have been shared
     album = db.StringProperty() # FB ID for this users Chillbrain album
+    linkbacks = db.IntegerProperty(default=0)
+    
+    def increment_linkback(self):
+        self.linkbacks += 1
+        self.put
     
     def vote(self, id):
         vote = Vote(user=self, img=id)
